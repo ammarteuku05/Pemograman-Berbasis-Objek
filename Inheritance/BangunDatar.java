118b6c06b0aae73313706c390fcfc26c2c4ebbce
ammarteuku05 <57586371+ammarteuku05@users.noreply.github.com>
2020-03-04 17:05:45 +0700

    Create BangunDatar.java

---
+++ Inheritance/BangunDatar.java
@@ -0,0 +1,9 @@
+package Inheritance;
+    public class BangunDatar {
+        public double Luas(){
+            return 0;
+        }
+       public double Keliling(){
+            return 0;
+        }
+}
