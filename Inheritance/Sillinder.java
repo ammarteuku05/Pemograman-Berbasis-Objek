69b297eeabe680233de2ef21482df409d8243d26
ammarteuku05 <57586371+ammarteuku05@users.noreply.github.com>
2020-03-04 21:44:09 +0700

    Update Sillinder.java

--- Inheritance/Sillinder.java
+++ Inheritance/Sillinder.java
@@ -1,13 +1,34 @@
 package Inheritance;
 public class Sillinder extends Lingkaran{
-    private double Tinggi;
+     private double Tinggi,phi=3.14,jariJari;
+
+    @Override
+    public double getPhi() {
+        return phi;
+    }
+
+    @Override
+    public void setPhi(double phi) {
+        this.phi = phi;
+    }
+
+    @Override
+    public double getJariJari() {
+        return jariJari;
+    }
+
+    @Override
+    public void setJariJari(double jariJari) {
+        this.jariJari = jariJari;
+    }
+
     public double getTinggi() {
         return Tinggi;
     }
     public void setTinggi(double Tinggi) {
-        Tinggi = this.Tinggi;
+        this.Tinggi = Tinggi;
     }
     public double Volume(){
-        return Luas() * this.Tinggi;
+        return phi *jariJari*jariJari * this.Tinggi;
     }
 }
